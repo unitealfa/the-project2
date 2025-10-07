514de2de955c0d2820707595d55a102daf9b83ed
unitealfa <Mourad.ADJOUT.dz@gmail.com>
2025-10-07 22:12:29 +0100

    aaaaaaa

--- back/src/app.ts
+++ back/src/app.ts
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
